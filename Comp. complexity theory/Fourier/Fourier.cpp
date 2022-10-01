159f232ee8d3f8319bd096ed4943a589cad92706
Никита Наумов <nikita.naumov.02@gmail.com>
2022-10-01 22:45:41 +0700

    Fourier base class added

---
+++ Comp. complexity theory/Fourier/Fourier.cpp
@@ -0,0 +1,29 @@
+#include "Fourier.h"
+
+namespace Fourier
+{
+void Fourier::Input()
+{
+    std::cout << "Input mass: ";
+    std::string a, b;
+    while (std::cin >> a >> b)
+    {
+        if (KeyboardInput::IsDigits(2, a, b))
+        {
+            std::complex<double> c(std::stod(a), std::stod(b));
+            m_complexList.push_back(c);
+        }
+            
+        if (std::cin.get() == '\n') return;
+    }
+}
+//------------------------------------------------------------
+void Fourier::Output()
+{
+    for (const auto& num : m_complexList)
+        std::cout << num;
+
+    std::cout << std::endl;
+}
+//------------------------------------------------------------
+}
---
+++ Comp. complexity theory/Fourier/Fourier.h
@@ -0,0 +1,27 @@
+﻿//-------------------------------------------------------------------------------------------
+// Description: Базовый класс для реализации преобразований Фурье
+// Date: 01.10.2022
+// Authors: Хьюго М.А. & Наумов Н.В.
+// Ⓒ Sibsutis university
+//-------------------------------------------------------------------------------------------
+
+#pragma once
+#include <vector>
+#include <complex>
+#include "../Input/KeyboardInput.h"
+
+namespace Fourier
+{
+class Fourier : public Input::KeyboardInput
+{
+	typedef std::vector<std::complex<double>> TComplexList;
+public:
+	Fourier() = default;
+	virtual ~Fourier() = default;
+
+	void Input() override;
+	virtual void Output();
+private:
+	TComplexList m_complexList;		// массив комплесных чисел
+};
+}
